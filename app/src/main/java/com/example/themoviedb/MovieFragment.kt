d378b3312ea43262859647e8edf9cf06d4236eb8
Ranggafebriowaleska <ranggafebrio61@gmail.com>
2023-07-13 09:07:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/themoviedb/MovieFragment.kt
@@ -0,0 +1,81 @@
+package com.example.themoviedb
+
+import Movie
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.themoviedb.model.MovieResponse
+import com.example.themoviedb.service.MovieApiInterface
+import com.example.themoviedb.service.MovieApiService
+import kotlinx.android.synthetic.main.fragment_movie.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class MovieFragment : Fragment() {
+    private val movies = arrayListOf<Movie>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_movie, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        rv_movies_list.layoutManager = LinearLayoutManager(this.context)
+        rv_movies_list.setHasFixedSize(true)
+        getMovieData { movies : List<Movie> ->
+            rv_movies_list.adapter = MovieAdapter(movies)
+        }
+        showRecyclerView()
+    }
+
+    private fun getMovieData(callback: (List<Movie>) -> Unit){
+        val apiService = MovieApiService.getInstance().create(MovieApiInterface::class.java)
+        apiService.getMovieList().enqueue(object : Callback<MovieResponse> {
+            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
+
+            }
+
+            override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>) {
+                return callback(response.body()!!.movies)
+            }
+
+        })
+    }
+
+    private fun showRecyclerView() {
+        rv_movies_list.layoutManager = LinearLayoutManager(this.context)
+        rv_movies_list.adapter = MovieAdapter(movies)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment HomeFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MovieFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
+}
